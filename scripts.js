48d4ca3e4509fad894ec710189802919bf7970c5
MrDruv <druv0806@gmail.com>
2025-05-22 15:51:22 +0530

    refactor

--- scripts.js
+++ scripts.js
@@ -1,3 +1,7 @@
+const taskInput = document.getElementById('taskInput');
+const taskList = document.getElementById('taskList');
+// stores todos here
+const todos=[];
 
 function render(todos, taskList){
         taskList.innerHTML='';
@@ -19,21 +23,17 @@ todos.forEach((task) => {
 
   function addTask(taskInput, todos, taskList) {
     const taskText = taskInput.value.trim();
-    if (taskText !== '') {
-       
+    if (taskText == '') {
+       alert("Please enter the task");
+       return
+    }
        todos.push(taskText);    // add to array
        render(todos, taskList);           //render updated list
        taskInput.value = '';
-    }else{
-        alert("Please enter the task")
-    }  
+    
   }
 
-document.addEventListener('DOMContentLoaded', () => {
-  const taskInput = document.getElementById('taskInput');
-  const taskList = document.getElementById('taskList');
-    // stores todos here
-    const todos=[];
+document.addEventListener('DOMContentLoaded', () => { 
   //Enter key
   taskInput.addEventListener('keydown', (e) => {
     if (e.key === 'Enter') {
